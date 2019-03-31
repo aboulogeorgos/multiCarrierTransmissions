c70eaac3f517fcca1e1b8f4d2b259a890fa6e044
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-03-31 07:51:08 +0300

    example2 added

---
+++ OFDM/Rayleigh/BPSK/OFDM_BPSK_Rayleigh_parallel_ex2.m
@@ -0,0 +1,56 @@
+%%
+% Copyright 2019, Alexandros-Apostolos A. Boulogeorgos, All rights reserved
+% 
+%%
+
+clear 
+close all
+clc
+
+tic
+
+resultsFilename='example2.mat';
+
+cd 'examples';
+    example2;
+cd ..
+
+cd 'functions'
+    printSimulationParameters; 
+    [ EsN0dB ] = SNRperBit2SNRperSymbol( EbN0dB,nDSC,nFFT );
+    [ simBER ] = zeros(size(EsN0dB));
+
+    myPool = parpool(4);
+    
+    parfor ii = 1:length(EbN0dB)
+        cd 'Tx'
+            [ ipBit,ipMod,ipModMapped,xF,xt,xtPlusCP ] = Tx( nBitPerSym,... 
+                                                nSym,nDSC,nFFT,nVSC,nCP );
+         cd ..
+         cd 'channel'
+            cd (channelType)
+                [ht,hF] = Rayleigh( nTap,nSym );
+            cd ..
+         cd ..
+         cd 'Rx'
+            [ rt,yOFDMsymbol,yt,yFbeforeEqualizer,yF,yMod,ipModHat,ipBitHat ] = ...
+                     Rx( ht,hF,xtPlusCP,nSym,nBitPerSym,nDSC,nFFT,nVSC,... 
+                     nTap,EsN0dB(ii));
+         cd ..
+         [ simBER(ii) ] = simulationBER( ipBit,ipBitHat );
+    end
+
+    [ theoryBER ] = theoreticalBER( EbN0dB );
+    figure
+    BERfigure( EbN0dB,simBER,theoryBER ); 
+cd .. 
+
+cd 'results'
+save(resultsFilename);
+
+cd ..
+
+delete(myPool);
+delete(gcp('nocreate')); 
+
+toc
--- OFDM/Rayleigh/BPSK/examples/example2.m
+++ OFDM/Rayleigh/BPSK/examples/example2.m
@@ -1,16 +1,15 @@
+simulationTitle = 'BER of OFDM in Rayleigh enviroment using BPSK modulation';
 %%
 % Copyright 2019, Alexandros-Apostolos A. Boulogeorgos, All rights reserved
 % 
 %%
-
-simulationTitle = 'BER of OFDM in Rayleigh enviroment using BPSK modulation';
 
 modulationType = 'BSPK';
 
 nFFT        = 64;   % fft size
 nDSC        = 52;   % number of data subcarriers
 nBitPerSym  = 52;   % number of bits per OFDM symbol (same as the number of subcarriers for BPSK)
-nSym        = 10^4; % number of symbols
+nSym        = 10^6; % number of symbols
 nCP         = 16;   % CP
 
 cd ..
@@ -21,7 +20,7 @@ cd ..
 
 cd 'examples'
 
-EbN0dB      = [0:35]; % bit to noise ratio
+EbN0dB      = [0:2:40]; % bit to noise ratio
 
 % channel
 channelType = 'Rayleigh';
