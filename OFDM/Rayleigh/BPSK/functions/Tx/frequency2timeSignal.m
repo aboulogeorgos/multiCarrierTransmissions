fa6272ec0924232ec4ce10ef846dc4c61e1c3468
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-04-09 22:55:36 +0300

    Description added

--- OFDM/Rayleigh/BPSK/functions/Tx/frequency2timeSignal.m
+++ OFDM/Rayleigh/BPSK/functions/Tx/frequency2timeSignal.m
@@ -1,13 +1,16 @@
 %%
-% Copyright 2019, Alexandros-Apostolos A. Boulogeorgos, All rights reserved
+% Copyright 2019, Alexandros-Apostolos A. Boulogeorgos. 
+% All rights reserved.
 % 
+%%
+%%
+% Description: This function transform the signal from the frequency to the
+% time domain by employing a simple inverse fast Fourrier tranformation.
 %%
 
 function [ xt ] = frequency2timeSignal( xF,nDSC,nFFT )
-    % Taking FFT, the term (nFFT/sqrt(nDSC)) is for normalizing the power of transmit symbol to 1 
+    % Taking FFT, the term (nFFT/sqrt(nDSC)) is for normalizing the power 
+    % of transmit symbol to 1 
    xt = (nFFT/sqrt(nDSC))*ifft(fftshift(xF.')).';
-
-
-
 end
 
