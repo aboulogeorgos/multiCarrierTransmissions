e4683b6fc5483945c43bc57b8a4034ad20b229a6
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-04-09 22:52:57 +0300

    Description added

--- OFDM/Rayleigh/BPSK/functions/Tx/BPSKmodulator.m
+++ OFDM/Rayleigh/BPSK/functions/Tx/BPSKmodulator.m
@@ -2,6 +2,10 @@
 % Copyright 2019, Alexandros-Apostolos A. Boulogeorgos, All rights reserved
 % 
 %%
+%%
+% Description: This function returns the BPSK modulated symbols. In more
+% detail, bit 0 is mapped to -1 and 1 to +1. 
+%%
 
 function [ ipMod ] = BPSKmodulator( ipBit )
     ipMod = 2*ipBit-1; % BPSK modulation 0 --> -1, 1 --> +1
