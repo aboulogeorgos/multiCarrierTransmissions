64d04eaa47229278c3e388850be08150d4f371a3
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-04-13 20:06:26 +0300

    Verification function added

---
+++ OFDM/Rayleigh/BPSK/functions/channel/pathloss/logNormalwithShadowingVerification.m
@@ -0,0 +1,19 @@
+function [m,PLdBmeanTheory, v, varTheory] = logNormalwithShadowingVerification( fc, fcUnits, d0, d0Units, L, n, d, dUnits, sigma, Ns, N )
+    
+    PLdB = zeros(1, Ns);
+
+for ii=1:1:Ns
+    [ PLdB(ii) ] = logNormalwithShadowing( fc, fcUnits, d0, d0Units, L, n, d, dUnits, sigma );
+end
+
+    m = mean(PLdB);
+    [ PLdBmeanTheory ] = logNormalPathlossModel( fc, fcUnits, d0, d0Units, L, n, d, dUnits );
+    
+    v = var(PLdB);
+    varTheory = sigma^2;
+    
+    figure
+    histogram(PLdB,N, 'normalization', 'pdf');
+
+end
+
