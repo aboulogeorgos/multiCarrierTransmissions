2deda0db29f94dd921c0322c881d795724b1ecac
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-04-13 20:48:31 +0300

    Example 1 updated

--- OFDM/Rayleigh/BPSK/functions/channel/pathloss/Examples/example1.m
+++ OFDM/Rayleigh/BPSK/functions/channel/pathloss/Examples/example1.m
@@ -13,25 +13,27 @@ cd ..
 [ PLdB2 ] = logNormalPathlossModel( fc, fcUnits, d0, d0Units, L, n, d, dUnits );
 cd('Examples')
 
-fc = 1;
-fcUnits = 'GHz';
-d0 = 1;
-d0Units='m';
-L = 0;
-n = 3;
-d=[0.1:0.1:100];
-dUnits='m';
 
-xl = ['d (', dUnits,')'];
+n = 3;
+
 
 cd ..
 [ PLdB3 ] = logNormalPathlossModel( fc, fcUnits, d0, d0Units, L, n, d, dUnits );
+cd('Examples')
+
+
+n = 4;
+
+
+cd ..
+[ PLdB4 ] = logNormalPathlossModel( fc, fcUnits, d0, d0Units, L, n, d, dUnits );
 cd('Examples')
 
 figure
 plot(d,PLdB2)
 hold on
 plot(d,PLdB3)
+plot(d,PLdB4)
 xlabel(xl)
 ylabel('Path loss (dB)')
-legend('n=2', 'n=3')
+legend('n=2', 'n=3', 'n=4')
