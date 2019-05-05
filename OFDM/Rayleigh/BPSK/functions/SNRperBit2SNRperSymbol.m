cd8f495dc3ccf882e2f65c119898079f4a972e3c
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-05-05 18:28:53 +0300

    [Description] Modified

--- OFDM/Rayleigh/BPSK/functions/SNRperBit2SNRperSymbol.m
+++ OFDM/Rayleigh/BPSK/functions/SNRperBit2SNRperSymbol.m
@@ -5,12 +5,16 @@
 
 %%
 % Description: It convernts the SNR per bit to the SNR per OFDM symbol
+% Input:
+%   EbN0dB: SNR per bit
+%     nDSC: number of active subcarriers
+%     nFFT: length of FFT
+%     nCP:  length of CP
+%%
 
 function [ EsN0dB ] = SNRperBit2SNRperSymbol( EbN0dB,nDSC,nFFT,nCP )
 
-EsN0dB      = EbN0dB + 10*log10(nDSC/nFFT) + 10*log10(nFFT/nCP); % converting to symbol to noise ratio
-
-
+    EsN0dB      = EbN0dB + 10*log10(nDSC/nFFT) + 10*log10(nFFT/nCP); % converting to symbol to noise ratio
 
 end
 
