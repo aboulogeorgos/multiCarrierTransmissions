45d92a53b7e31c53b383541eec80747207ba014d
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-05-05 18:18:33 +0300

    [Description] Added

--- OFDM/Rayleigh/BPSK/functions/channel/pathloss/net2dBConverter.m
+++ OFDM/Rayleigh/BPSK/functions/channel/pathloss/net2dBConverter.m
@@ -2,6 +2,13 @@
 % Copyright 2019, Alexandros-Apostolos A. Boulogeorgos, All rights reserved
 % 
 %%
+%%
+% Description: This function convert a net number to dB
+% Input:
+%   net: net number
+% Output:
+%    dB: number in dB 
+%%
 
 function [ dB ] = net2dBConverter( net )
     dB = 10*log10(net);
