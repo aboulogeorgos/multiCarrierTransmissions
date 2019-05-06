b1888843bc470e10a29fb2faa5246074248229d4
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-05-06 07:28:04 +0300

    [Description] Added/modified

--- OFDM/Rayleigh/BPSK/functions/BERfigure.m
+++ OFDM/Rayleigh/BPSK/functions/BERfigure.m
@@ -1,11 +1,16 @@
 %%
-% Copyright 2019, Alexandros-Apostolos A. Boulogeorgos, All rights reserved
+% Copyright 2019, Alexandros-Apostolos A. Boulogeorgos, All rights
+% reserved.
 % 
 %%
 
 %%
 % Description: This function plots the theoretical and simulation bit error
 % rate (BER) in a common figure.
+% Input:
+%   EbN0dB: SNR per bit in dB
+%   simBER: simulation BER
+%   theoryBER: theoretical BER
 %%
 function [ ] = BERfigure( EbN0dB,simBER,theoryBER )
     semilogy(EbN0dB,theoryBER,'bs-','LineWidth',2);
