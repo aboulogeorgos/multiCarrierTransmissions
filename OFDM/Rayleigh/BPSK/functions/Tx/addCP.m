12d8d8deec483686f6de935cdba4feb5bbd724a5
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-05-06 07:34:31 +0300

    [Description] Added/Modified

--- OFDM/Rayleigh/BPSK/functions/Tx/addCP.m
+++ OFDM/Rayleigh/BPSK/functions/Tx/addCP.m
@@ -4,6 +4,11 @@
 %%
 %% 
 % Description: This functions add cyclic prefix (CP) to the OFDM signal 
+% Input:
+%   nCP: CP length
+%   xt:  signal in time domain w/o CP
+% Output:
+%   xt:  signal in time domain with CP
 %%
 
 function [ xt ] = addCP( nCP,xt )
